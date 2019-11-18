cd972228c0b6e0e0b4da319cb76ce4bb4209f618
seleuchel <smsky1010@gmail.com>
2019-11-18 20:54:44 +0900

    kernel info

--- Analyze_OS.py
+++ Analyze_OS.py
@@ -26,6 +26,7 @@ def get_nt_security():
 
 
 def get_posix_security():
+    kernel = check_output('uname -r', encoding='utf-8')
     glibc = check_output('ldd --version | grep LIBC', encoding='utf-8').split()[-1]
     rvs = os.system('cat /proc/sys/kernel/randomize_va_space', encoding='utf-8')
     if rvs == '2':
@@ -36,6 +37,7 @@ def get_posix_security():
         aslr = False
 
     return {
+        'KERNEL': kernel,
         'GLIBC': glibc,
         'ASLR': aslr
     }
